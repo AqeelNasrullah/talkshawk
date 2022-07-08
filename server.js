4a652fbf64120bb802a976652589a9222d73faf9
Aqeel Nasrullah <aqeelnasrullah1997@gmail.com>
2022-07-08 13:48:14 +0500

    Deployment: Updated heroku-postbuild script | Backend: client delivery through backend

--- server.js
+++ server.js
@@ -2,13 +2,38 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+const logger = require("morgan");
+const path = require("path");
 
 global.onlineUsers = {};
 
 const PORT = process.env.PORT || 8000;
+const BASE_URL = process.env.BASE_URL || "/api";
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors({ origin: "*", credentials: true }));
+app.use(cors(logger("dev")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get(BASE_URL, (req, res) => {
+  res.status(200).json({ message: "Server is up & running..." });
+});
+
+app.use(express.static(path.join(__dirname, "client", build)));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening to *:${PORT}`);
+});
+
+// sockets
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -26,8 +51,4 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(socket.id + " disconnected");
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening to *:${PORT}`);
 });
